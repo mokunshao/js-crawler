e8671e4e2dd13f7418c184cde3ca00fb0ff3b76f
mokunshao <mokunshao@foxmail.com>
2020-02-29 02:20:14 +0800

    backup

---
+++ index.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const Movie = function () {
+  this.name = '';
+  this.score = 0;
+  this.quote = '';
+  this.ranking = 0;
+  this.coverUrl = '';
+};
+
+const main = function () {
+
+};
+
+main();
